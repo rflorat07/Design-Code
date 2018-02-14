7f88264d9a60c4dbbf1859ecf206dec4cb9a789a
Roger Florat <rflorat@cidiverte.it>
2018-02-14 16:43:50 +0100

    Initial Commit

---
+++ DesignCodeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignCodeApp
+//
+//  Created by Roger Florat on 14/02/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

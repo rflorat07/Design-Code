3886e150e732c5e1fba6133a0552ab6c940a8a65
Roger Florat <rflorat@cidiverte.it>
2018-02-16 16:27:16 +0100

    Container View - Simplify your workflow child view controllers

---
+++ DesignCodeApp/TestimonialCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TestimonialCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Roger Florat on 16/02/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class TestimonialCollectionViewCell: UICollectionViewCell {
+    
+    // Outlet
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+
+}

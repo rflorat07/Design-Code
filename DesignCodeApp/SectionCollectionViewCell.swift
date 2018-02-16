ecaf1bbe1c5ee528bc2a3ed8ed2edda114663f70
Roger Florat <rflorat@cidiverte.it>
2018-02-16 12:40:47 +0100

    Static Data - Filling your reusable cells with data

---
+++ DesignCodeApp/SectionCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCodeApp
+//
+//  Created by Roger Florat on 16/02/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    
+    // Outlet
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
